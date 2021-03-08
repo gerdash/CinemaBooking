0c8c4907efc96069393a975e48593f6d05dc6c7a
Gerda <gerda.sadurska@gmail.com>
2021-03-08 17:30:29 +0000

    Add project files.

---
+++ CinemaLogic/CinemaLogic/CinemaManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CinemaLogic
+{
+    public class CinemaManager
+    {
+       
+    }
+}
---
+++ ConsoleTest/CinemaTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CinemaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nananan!");
+            Console.WriteLine("Whooooooo");
+            Console.WriteLine("Whaaaaaaaa");
+        }
+    }
+}
