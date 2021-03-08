b292e5ead5b0fbe46ca618f5b8e4839fb3cfe163
Gerda <gerda.sadurska@gmail.com>
2021-03-08 19:01:21 +0000

    Console testing for the first two functions

--- ConsoleTest/CinemaTest/Program.cs
+++ ConsoleTest/CinemaTest/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using CinemaLogic;
+using CinemaLogic.Managers;
 
 namespace CinemaTest
 {
     class Program
     {
+        private static CategoryManager categories = new CategoryManager();
+        private static CinemaManager films = new CinemaManager();
         static void Main(string[] args)
         {
-            Console.WriteLine("Nananan!");
-            Console.WriteLine("Whooooooo");
-            Console.WriteLine("Whaaaaaaaa");
-            //this is a comment
+            Console.WriteLine("Categories: ");
+            categories.GetAllCategories().ForEach(n =>
+            {
+                Console.WriteLine(n.Name);
+            });
+
+            Console.WriteLine("Films: ");
+            films.GetAllFilms().ForEach(f =>
+            {
+                Console.WriteLine(f.Title);
+            });
+
         }
     }
 }
